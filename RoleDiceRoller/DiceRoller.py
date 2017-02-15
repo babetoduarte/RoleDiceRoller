3e907f4875d0b2e4732852fc9557c9dde62048c1
Jorge A. Duarte <babetoduarte@gmail.com>
2017-02-14 23:53:01 -0500

    Added multi-dice and +/- extra modifier capabilities according to common notation (ie. 3d5+2, 4d6-3, etc...)

--- RoleDiceRoller/DiceRoller.py
+++ RoleDiceRoller/DiceRoller.py
@@ -1,10 +1,37 @@
 from RoleDiceRoller.DiceBag import DiceBag
 
 db = DiceBag()
-db.add_die(4, 'd4')
-print("Rolling 3d4:")
-print(db.roll_dice(3, 4))
 
-db.add_die(20, 'd20')
-print("Rolling 4d20:")
-print(db.roll_dice(4, 20))
+def roll(ndice):
+    extra = 0
+    info1 = ndice.split('d')
+    amount = int(info1[0])
+    if '+' in ndice:
+        sides = int(info1[1].split('+')[0])
+        extra = int(info1[1].split('+')[1])
+    elif '-' in ndice:
+        sides = int(info1[1].split('-')[0])
+        extra = int(info1[1].split('-')[1])
+        extra *= -1
+    else:
+        sides = int(info1[1])
+
+    db.add_die(sides)
+    roll_results = db.roll_dice(amount, sides)
+    roll_sum = sum(roll_results)
+
+    if extra > 0:
+        print("Rolling "+str(amount)+"d"+str(sides) + "+" + str(extra) + ":")
+        print(str(roll_results) + "+" + str(extra) + "=" + str(roll_sum + extra))
+    elif extra < 0:
+        print("Rolling " + str(amount) + "d" + str(sides) + str(extra) + ":")
+        print(str(roll_results) + str(extra) + "=" + str(roll_sum + extra))
+    else:
+        print("Rolling " + str(amount) + "d" + str(sides) + ":")
+        print(str(roll_results) + "=" + str(roll_sum))
+
+
+roll('5d6')
+roll('2d20')
+roll('5d7+3')
+roll('3d8-2')
